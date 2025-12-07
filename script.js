dd899ce61e16ccccd3a9b7853ed92fd29a519950
Lucas Vasconcelos <164563464+y2lucas@users.noreply.github.com>
2025-12-07 14:00:56 -0300

    salve1

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-oper").addEventListener("click", () => {
+    const nav = document.querySelector(".menu nav");
+    nav.style.display = nav.style.display === "block" ? "none" : "block";
+});
